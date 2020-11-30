7db1197b131356a151769bd963e81f0d27f23145
Ibtid <ibtid011@example.com>
2020-11-30 19:29:26 +0600

    same

--- src/SharedComponents/UIElements/Modal/ProductModal/Modal.js
+++ src/SharedComponents/UIElements/Modal/ProductModal/Modal.js
@@ -2,39 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from '../../Buttons/Buttons';
 import CloseIcon from '@material-ui/icons/Close';
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 
 import './Modal.css';
 
 const Modal = (props) => {
   const closeIcon = <CloseIcon />;
   return ReactDOM.createPortal(
-    <div className='modal'>
-      <motion.div
-        animate={{ y: 430 }}
-        className='modal__container'
-        onClick={() => {}}>
-        <div className='modal__imageContainer'>
-          <img className='modal__image' src={props.image} alt='product' />
-        </div>
-        <div className='modal__info'>
-          <div className='modal__title'>{props.name}</div>
-          <div className='modal__price'>Price: ${props.price}</div>
-          <div className='modal__description'>
-            <div className='modal__descriptionTitle'>Description</div>
-            <div className='modal__descriptionText'>{props.description}</div>
+    <AnimatePresence>
+      <div className='modal'>
+        <motion.div
+          animate={{ y: 430 }}
+          className='modal__container'
+          onClick={() => {}}>
+          <div className='modal__imageContainer'>
+            <img className='modal__image' src={props.image} alt='product' />
           </div>
-          <div className='modal__buttons'>
-            <Button
-              icon={closeIcon}
-              onClick={props.setIsOpen}
-              type='danger'
-              message='Close'
-            />
+          <div className='modal__info'>
+            <div className='modal__title'>{props.name}</div>
+            <div className='modal__price'>Price: ${props.price}</div>
+            <div className='modal__description'>
+              <div className='modal__descriptionTitle'>Description</div>
+              <div className='modal__descriptionText'>{props.description}</div>
+            </div>
+            <div className='modal__buttons'>
+              <Button
+                icon={closeIcon}
+                onClick={props.setIsOpen}
+                type='danger'
+                message='Close'
+              />
+            </div>
           </div>
-        </div>
-      </motion.div>
-    </div>,
+        </motion.div>
+      </div>
+    </AnimatePresence>,
     document.getElementById('portal')
   );
 };
