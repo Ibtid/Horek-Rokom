8a500ed5aaffd2ede1da24d7f4c7fce27437b748
Ibtid <ibtid011@example.com>
2020-12-10 15:37:36 +0600

    create server

--- src/Admin/Pages/Admin/Admin.js
+++ src/Admin/Pages/Admin/Admin.js
@@ -7,17 +7,15 @@ import { Link } from 'react-router-dom';
 const Admin = () => {
   return (
     <div className='admin__main'>
-      <div className='login'>
-        <div className='login__container'>
-          <Logo />
-          <div className='admin__main__text'>
-            <h1>ADMIN PANEL</h1>
-          </div>
-          <Link to='/admin/control' className='admin__Link'>
-            <Button type='danger' message='Go to Admin Control' />
-          </Link>
-          <Button type='danger' message='Go to Orders List' />
+      <div className='login__container'>
+        <Logo />
+        <div className='admin__main__text'>
+          <h1>ADMIN</h1>
         </div>
+        <Link to='/admin/control' className='admin__Link'>
+          <Button type='danger' message='Go to Store' />
+        </Link>
+        <Button type='danger' message='Go to Orders List' />
       </div>
     </div>
   );
